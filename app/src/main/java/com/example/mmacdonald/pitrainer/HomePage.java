3cc947bf8424caa8db2ce12ab3537c28ea0e55d1
Martin-MacDonald <mmacdo54@caledonian.ac.uk>
2017-09-04 15:47:55 +0100

    Added logout functionality

--- app/src/main/java/com/example/mmacdonald/pitrainer/HomePage.java
+++ app/src/main/java/com/example/mmacdonald/pitrainer/HomePage.java
@@ -1,9 +1,41 @@
 package com.example.mmacdonald.pitrainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomePage extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.extra_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                Intent showSignInPage = new Intent(this, MainActivity.class);
+                startActivity(showSignInPage);
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/mmacdonald/pitrainer/MainActivity.java
+++ app/src/main/java/com/example/mmacdonald/pitrainer/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -106,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -118,11 +122,7 @@ public class MainActivity extends AppCompatActivity {
 
         mShowHomePageIntent= new Intent(getApplicationContext(), HomePage.class);
 
-        DatabaseReference dbref = FirebaseDatabase.getInstance().getReference();
-
         mAuth = FirebaseAuth.getInstance();
-
-        mAuth.signOut();
 
         if (mAuth.getCurrentUser() != null){
 
