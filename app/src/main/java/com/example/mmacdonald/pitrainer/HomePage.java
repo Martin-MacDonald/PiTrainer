8b6b9fb576f6cbf9a64c0e2228d76f383ddbc207
Martin-MacDonald <mmacdo54@caledonian.ac.uk>
2017-09-04 16:24:44 +0100

    Changed ActionBar to Toolbar

--- app/src/main/java/com/example/mmacdonald/pitrainer/HomePage.java
+++ app/src/main/java/com/example/mmacdonald/pitrainer/HomePage.java
@@ -1,8 +1,11 @@
 package com.example.mmacdonald.pitrainer;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -41,5 +44,11 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        Toolbar homePageToolbar = (Toolbar) findViewById(R.id.homePageToolbar);
+        setSupportActionBar(homePageToolbar);
+
+        getSupportActionBar().setBackgroundDrawable(ContextCompat.getDrawable(this, android.R.color.transparent));
+
     }
 }
